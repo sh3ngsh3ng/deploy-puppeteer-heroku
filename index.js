30453f198c8f29f2dc145f6425ea1621a6d26fe1
ys <ysdeveloper01@gmail.com>
2022-06-03 01:42:35 +0800

    added login for insta

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const puppeteer = require("puppeteer")
 
 let app = express();
 
@@ -8,6 +8,48 @@ async function main() {
   app.get("/", (req,res)=> {
     res.status(200)
     res.send("Server is up and running")
+  })
+
+  app.get("/login", (req,res)=> {
+    async function loginInstagram() {
+      // open up browser (headless = false opens up the browser for view)
+      const browser = await puppeteer.launch({
+        'args' : [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      })
+      // incognito mode
+      const context = await browser.createIncognitoBrowserContext()
+      // new tab
+      const page = await context.newPage()
+      // go to link
+      await page.goto("https://www.instagram.com/", {
+          waitUntil: 'networkidle2'
+      })
+      // fill in login details
+      await page.type("input[name=username]", process.env.INSTA_USERNAME)
+      await page.type("input[name=password]", process.env.INSTA_PASSWORD)
+      // submit login form
+      await page.click("button[type=submit]")
+      await page.waitForNavigation()
+      // refresh instagram page
+      await page.click("a[href='/']")
+      await page.waitForNavigation()
+      // click on popup
+      const [button] = await page.$x("//button[.='Not Now']")
+      if (button) {
+          await button.click()
+      }
+      // screenshot page
+      // await page.screenshot({
+      //     path: "inputtest.png"
+      // })
+  
+      await browser.close()
+  }
+  
+  loginInstagram()
   })
 
   
